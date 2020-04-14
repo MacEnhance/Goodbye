15d40eca5cc5110464ec8c0007a44a01c28e2021
Wolfgang Baird <aguywithlonghair@yahoo.com>
2020-04-14 13:38:15 -0700

    Initial commit

---
+++ Goodbye/Goodbye.m
@@ -0,0 +1,26 @@
+//
+//  Goodbye.m
+//  Goodbye
+//
+//  Created by Wolfgang Baird on 4/14/20.
+//  Copyright © 2020 Wolfgang Baird. All rights reserved.
+//
+
+#import "ZKSwizzle.h"
+#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
+@interface ME_Goodbye_t : NSObject <NSApplicationDelegate>
+@end
+
+@implementation ME_Goodbye_t
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender { return true; }
+@end
+
+@interface Goodbye : NSObject
+@end
+
+@implementation Goodbye
++ (void)load { _ZKSwizzle(ME_Goodbye_t.class, NSApp.delegate.class); }
+@end
+
